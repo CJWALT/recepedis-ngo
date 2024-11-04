1f151b0dff0d38fe4fc411ced68e09589f71b29a
cjwalt <oyedelee22@gmail.com>
2024-11-04 05:34:21 -0800

    remove border red from about us section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -8,7 +8,7 @@ function AboutUs() {
   return (
     <>
         <section >
-          <div className='bg-[#E5E5E5] w-[90%] sm:w-[80%] md:w-[90%] mx-auto border-2 border-red-400'>
+          <div className='bg-[#E5E5E5] w-[90%] sm:w-[80%] md:w-[90%] mx-auto'>
           <div className='py-[3rem]'>
               <h3 className='text-[1.3rem] sm:text-[1.7rem] font-medium text-center text-txtColor'>More Than $100 Million Donateds since 2015</h3>
               <p className='font-light text-center text-[.8rem] sm:text-[1rem]'>
