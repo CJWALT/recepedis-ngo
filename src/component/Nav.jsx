fe55414f2012174f2388596c993212310480ab30
cjwalt <oyedelee22@gmail.com>
2024-11-03 16:20:45 -0800

    feat: enhance NotFound page with additional content and 'Go Back Home' link

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -12,8 +12,8 @@ import Button from './Button'
 const links = [
         {id:1, name:'Home', path:'/'},
         {id:2, name:'AboutUs', path:'/about-us'},
-        {id:3, name:'Contact us', path:'/contact'},
-        {id:4, name:'Our Activities', path:'/ouractivities'}
+        {id:3, name:'Contact us', path:'/constact'},
+        {id:4, name:'Our Activities', path:'/ouractfivities'}
       ]
 
       
@@ -27,9 +27,9 @@ function Nav() {
 
   return (
     <div className='py-[1.3rem] flex flex-row justify-between w-[90%] max-w-[1280px] mx-auto'>
-      <div className=''>
+      <Link to='/' className=''>
             <img src={Logo} alt='logo' className='object-contain w-[92px] h-[89px]'/>
-      </div>
+      </Link>
      <div className={` justify-between z-30  items-center transition-opacity duration-500 ease-in ${openMenu ? ' flex absolute left-0 transition-opacity opacity-100 duration-900 ease-in-out top-0 rounded-lg w-[13rem] bg-[#6EB6E9] py-6 flex-col' : 'opacity-0 hidden lg:opacity-100 lg:flex lg:w-[45rem] lg:max-w[41rem]'}`}>
          <ul className='w-[70%] lg:w-[72%]'>
           <div className='flex flex-col lg:flex-row items-center gap-4 lg:pb-0 pb-[2rem] justify-between'>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <div>
-      notfound
+      <h3>Sorry, page not found!</h3>
+      <Link to='/' className='text-btnColor'>Go back to homepage</Link>
     </div>
   )
 }
