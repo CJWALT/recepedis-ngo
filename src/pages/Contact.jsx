6e8ab61bdbd4a0c20a7f16bbb8381feee10d57cc
cjwalt <oyedelee22@gmail.com>
2024-12-13 02:55:45 -0800

    ui/ux: add contact us styles, add logic  to send message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,10 @@
+
 import React, { useState } from 'react'
+import Button from '../component/Button'
+
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+
 
 function Contact() {
 
@@ -32,30 +38,49 @@ function Contact() {
   }
  
   return (
-    <>
-    <div className='w-[90%] sm:w-[80%] md:w-[90%] mx-auto border-2 border-red-400 py-[6rem]'>
+    <div className='relative'>
+       <span className='bg-gray-400 -z-10 absolute h-[50%] bottom-0 w-full block'>
+        </span>
+            <div className='w-[90%] sm:w-[80%] md:w-[90%] mx-auto border-2 border-red-400 py-[3rem]'>
+              <div className='border-2 border-red-400 flex flex-col'>
+                  <h4 className='font-bold mb-[.8rem] text-[2rem]'>Get in Touch </h4>
 
-    <h4>Contact us at RECEPEDIS today!</h4>
-
-      <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-            <input type='text' value={name} placeholder='Name:' onChange={(e) => setName(e.target.value)} required/>
-
-            <label htmlFor="email">
-              Email: 
-            </label>
-            <input type='email' value={email} placeholder='Email:' onChange={(e) => setEmail(e.target.value)} required/>
-            
-            <label htmlFor="message">Message:</label>
-            <textarea name="message" value={message} id="message" onChange={(e) => setMessage(e.target.value)}>
-              Enter message here
-            </textarea>
-            <button type='submit'>Send message</button>
-              </form>  
+                <div className='px-4 py-7 bg-white shadow-lg'>
+                  <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                    
+                      <input type='text' value={name} className='border-2 border-black rounded-md h-12' placeholder='Name:' onChange={(e) => setName(e.target.value)} required/>
+                    <input type='email' value={email} className='border-2 border-black rounded-md h-12' placeholder='Email:' onChange={(e) => setEmail(e.target.value)} required/>
+                    <textarea name="message" className='resize-none border-2 border-black rounded-md focus:outline-none' placeholder='Enter Message' rows={4} cols={3} value={message} id="message" onChange={(e) => setMessage(e.target.value)}>
+                    </textarea>
+                    
+                      <div>
+                      <Button
+                      children='Send Message'
+                      className='bg-btnColor hover:bg-white rounded-lg'/>
+                        </div>          </form>  
+                </div>
+                <div className='border-2 border-red-400 py-4'>
+                <h5 className='mb-2'> Stay Connected with us on our Socials: </h5> 
+                  <div className='mb-4 flex gap-[1rem]'>
+                    <span>
+                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
+                    </span>
+                    <span>
+                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
+                    </span>
+                    <span>
+                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
+                    </span>
+                    <span>
+                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
+                    </span>
+                    </div>
+                </div>
               
+              </div>
             </div>
       
-    </>
+    </div>
   )
 }
 
