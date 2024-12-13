3189f98e9fab0f695b9013e5b57aa855c703b5c1
cjwalt <oyedelee22@gmail.com>
2024-12-13 03:21:12 -0800

    add social icon and address

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,8 @@ import Button from '../component/Button'
 
 
 import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 
 function Contact() {
@@ -39,10 +41,10 @@ function Contact() {
  
   return (
     <div className='relative'>
-       <span className='bg-gray-400 -z-10 absolute h-[50%] bottom-0 w-full block'>
+       <span className='bg-[#010104cc] -z-10 absolute h-[50%] bottom-0 w-full block'>
         </span>
-            <div className='w-[90%] sm:w-[80%] md:w-[90%] mx-auto border-2 border-red-400 py-[3rem]'>
-              <div className='border-2 border-red-400 flex flex-col'>
+            <div className='w-[90%] sm:w-[80%] md:w-[90%] mx-auto py-[3rem]'>
+              <div className='flex flex-col'>
                   <h4 className='font-bold mb-[.8rem] text-[2rem]'>Get in Touch </h4>
 
                 <div className='px-4 py-7 bg-white shadow-lg'>
@@ -57,23 +59,28 @@ function Contact() {
                       <Button
                       children='Send Message'
                       className='bg-btnColor hover:bg-white rounded-lg'/>
-                        </div>          </form>  
+                        </div>        
+                          </form>  
                 </div>
-                <div className='border-2 border-red-400 py-4'>
-                <h5 className='mb-2'> Stay Connected with us on our Socials: </h5> 
+                <div className='pt-6 mt-[2rem] text-white'>
+                <h5 className='mb-2'> Stay Connected with us: </h5> 
                   <div className='mb-4 flex gap-[1rem]'>
                     <span>
                     <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
                     </span>
                     <span>
-                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
+                    <a href="https://www.facebook.com/profile.php?id=100079275348605&_rdc=1&_rdr" target="_blank" rel="noopener noreferrer"><FacebookIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>
+                    </span>
+                    <span>
+                    <a href="https://wa.me/+237677087801" target="_blank" rel="noopener noreferrer"><WhatsAppIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>
+
                     </span>
                     <span>
                     <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
                     </span>
-                    <span>
-                    <a href="https://instagram.com/recepedis237?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer"><InstagramIcon className='!w-[2rem] !h-[2rem] px-[.3rem] py-[.3rem] bg-[#E9E9E9] rounded-full text-[#121A6B]'/></a>  
-                    </span>
+                    </div>
+                    <div>
+                      <h5 className='mt-2 text-[1rem] font-light'>Mile 90, Nsongwa Bamenda Cameroon.</h5>
                     </div>
                 </div>
               
